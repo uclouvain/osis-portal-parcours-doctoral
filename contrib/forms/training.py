04a84f9fa4a90292e0bad539f47372e85550ae1d
Gagaro <gagaro42@gmail.com>
2025-04-07 10:36:07 +0200

    Fix OS-1398: Parcours Doctoral - Retours de tests mars

--- contrib/forms/training.py
+++ contrib/forms/training.py
@@ -33,11 +33,12 @@ from django.utils.translation import get_language
 from django.utils.translation import gettext_lazy as _
 from django.utils.translation import pgettext_lazy
 
-from base.models.academic_year import current_academic_year
 from osis_parcours_doctoral_sdk.model.parcours_doctoral_dto import ParcoursDoctoralDTO
 from osis_parcours_doctoral_sdk.model.type_enum import (
     TypeEnum as PaperTypeEnum,
 )
+
+from base.models.academic_year import current_academic_year
 from parcours_doctoral.contrib.enums.training import (
     ChoixComiteSelection,
     ChoixRolePublication,
@@ -46,11 +47,13 @@ from parcours_doctoral.contrib.enums.training import (
     ChoixTypeVolume,
     ContexteFormation,
 )
-from parcours_doctoral.contrib.forms import DoctorateFileUploadField as FileUploadField, get_country_initial_choices
 from parcours_doctoral.contrib.forms import (
     EMPTY_CHOICE,
     BooleanRadioSelect,
     CustomDateInput,
+)
+from parcours_doctoral.contrib.forms import DoctorateFileUploadField as FileUploadField
+from parcours_doctoral.contrib.forms import (
     SelectOrOtherField,
     autocomplete,
     get_country_initial_choices,
@@ -273,7 +276,7 @@ class ConferenceForm(ActivityFormMixin, forms.Form):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['title'].help_text = _("Name in the language of the manifestation")
+        self.fields['title'].help_text = _("Please specify the title in the language of the manifestation")
         self.fields['participating_days'].help_text = _(
             "Please specify either a hourly volume or a number of participating days"
         )
@@ -404,6 +407,9 @@ class ConferencePublicationForm(ActivityFormMixin, forms.Form):
 
         super().__init__(*args, **kwargs)
 
+        self.fields['authors'].help_text = _(
+            'Please use the following format for inputting the first and last name: "Monteiro, M. et Marti, A. C."'
+        )
         self.fields['publication_status'].help_text = _("Refer to the website of your commission for more details.")
         self.fields['acceptation_proof'].help_text = _(
             "Submit a proof, for example a letter from the editor,"
@@ -429,7 +435,7 @@ class CommunicationForm(ActivityFormMixin, forms.Form):
         ],
     )
     subtitle = forms.CharField(
-        label=_("Title of the communication"),
+        label=_("Communication title (in the activity language)"),
         max_length=200,
         required=False,
     )
@@ -463,7 +469,6 @@ class CommunicationForm(ActivityFormMixin, forms.Form):
         ]
         labels = {
             'title': _("Event name"),
-            'subtitle': _("Communication title (in the activity language)"),
             'start_date': _("Activity date"),
             'website': _("Event website"),
             'acceptation_proof': _("Proof of acceptation by the committee"),
@@ -541,6 +546,9 @@ class PublicationForm(ActivityFormMixin, forms.Form):
 
         super().__init__(*args, **kwargs)
 
+        self.fields['authors'].help_text = _(
+            'Please use the following format for inputting the first and last name: "Monteiro, M. et Marti, A. C."'
+        )
         self.fields['publication_status'].help_text = _(
             "Specify the status of the publication or of the patent. Consult the website of your commission for "
             "more detail."
